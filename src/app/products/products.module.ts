274e8e202c48563865f04b17ca95dfaf30b89e4c
Adrian Camarena <adriancamarena2310mail.com>
2023-11-20 15:37:32 -0600

    video-171 ToogleCasePipe - Piper Personalizado

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -6,18 +6,20 @@ import { UncommonPageComponent } from './pages/uncommonPage/uncommonPage.compone
 import { NumbersPageComponent } from './pages/numbersPage/numbersPage.component';
 import { BasicsPageComponent } from './pages/basicsPage/basicsPage.component';
 import { OrderComponent } from './pages/order/order.component';
+import { ToggleCasePipe } from './pipes/roggle-case.pipe';
 
 @NgModule({
   imports: [
     CommonModule,
     ProductsRoutingModule,
-    PrimeNGModule
+    PrimeNGModule,
   ],
   declarations: [
     UncommonPageComponent,
     NumbersPageComponent,
     BasicsPageComponent,
-    OrderComponent
+    OrderComponent,
+    ToggleCasePipe,
   ]
 })
 export class ProductsModule { }
